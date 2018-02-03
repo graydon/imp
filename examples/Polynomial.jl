bec3ab0281ff8624199fbcbf32302a4044d01023
Jamie Brandon <jamie@scattered-thoughts.net>
2018-02-03 00:18:58 +0000

    Remove @time

--- examples/Polynomial.jl
+++ examples/Polynomial.jl
@@ -12,7 +12,7 @@ const yy = Relation((collect(1:1000000), collect(1:1000000)), 1)
 # have to put this in a separate function because LegacyParser is a bit crap
 zz(x,y) = (x * x) + (y * y) + (3 * x * y)
 
-@time function f(xx, yy) 
+function f(xx, yy) 
   @query begin
     xx(i, x)
     yy(i, y)
